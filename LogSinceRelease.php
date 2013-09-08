2e21309950bc5b07ae12abaea495edee76204184
Paul M. Jones <pmjones88@gmail.com>
2013-09-08 15:36:40 -0500

    LogSinceRelease now has cleaner output, allows a package argument

--- LogSinceRelease.php
+++ LogSinceRelease.php
@@ -3,15 +3,24 @@ class LogSinceRelease extends AbstractCommand
 {
     public function __invoke(array $argv)
     {
-        $version = $this->shell('git tag --list');
-        $last = $this->shell("git show {$version} --pretty=format:'%H %ci'");
+        $dir = array_shift($argv);
+        if ($dir) {
+            $orig = getcwd();
+            chdir($dir);
+        }
+        
+        $version = exec('git tag --list');
+        $last = exec("git show {$version} --pretty=format:'%H %ci'");
         list($hash, $date, $time, $zone) = explode(' ', $last);
-        $message = "Last release was {$version} on {$date} at {$time} {$zone}";
+        $package = basename(getcwd());
+        $message = "$package: last release was {$version} on {$date} at {$time} {$zone}";
         $this->outln($message);
-        $this->outln('');
-        $this->outln('--------');
-        $this->outln('');
+        $this->outln();
         $after = "$date $time $zone";
-        $this->shell("git log --name-status --reverse --after='{$after}'");
+        passthru("git log --name-status --reverse --after='{$after}'");
+        
+        if ($orig) {
+            chdir($orig);
+        }
     }
 }
