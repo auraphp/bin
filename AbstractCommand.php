526a0ece69828bcd8ed37a260aef1b52c646f91a
Paul M. Jones <pmjones88@gmail.com>
2014-08-23 12:06:38 -0500

    whitespace

--- AbstractCommand.php
+++ AbstractCommand.php
@@ -2,22 +2,22 @@
 abstract class AbstractCommand
 {
     protected $config;
-    
+
     public function __construct(Config $config)
     {
         $this->config = $config;
     }
-    
+
     protected function out($str = null)
     {
         echo $str;
     }
-    
+
     protected function outln($str = null)
     {
         $this->out($str . PHP_EOL);
     }
-    
+
     protected function shell($cmd, &$output = null, &$return = null)
     {
         $cmd = str_replace('; ', ';\\' . PHP_EOL, $cmd);
@@ -29,13 +29,13 @@ abstract class AbstractCommand
         }
         return $result;
     }
-    
+
     protected function api($method, $path, $body = null)
     {
         $github_auth = $this->config->github_user
                      . ':'
                      . $this->config->github_token;
-                     
+
         $api = "https://{$github_auth}@api.github.com";
         $url = $api . $path;
         $context = stream_context_create([
@@ -53,12 +53,12 @@ abstract class AbstractCommand
         $data = json_decode($json);
         return $data;
     }
-    
+
     protected function isReadableFile($file)
     {
         return file_exists($file) && is_readable($file);
     }
-    
+
     protected function apiGetRepos()
     {
         $data = $this->api('GET', '/users/auraphp/repos');
@@ -69,7 +69,7 @@ abstract class AbstractCommand
         ksort($repos);
         return $repos;
     }
-    
+
     protected function apiGetTags($repo)
     {
         $data = $this->api('GET', "/repos/auraphp/$repo/tags");
@@ -80,7 +80,7 @@ abstract class AbstractCommand
         usort($tags, 'version_compare');
         return $tags;
     }
-    
+
     protected function apiGetIssues($name)
     {
         return $this->api('GET', "/repos/auraphp/{$name}/issues");
@@ -95,42 +95,42 @@ abstract class AbstractCommand
         }
         return trim($branch);
     }
-    
+
     protected function isValidVersion($version)
     {
         $format = '^(\d+.\d+.\d+)(-(dev|alpha\d+|beta\d+|RC\d+))?$';
         preg_match("/$format/", $version, $matches);
         return (bool) $matches;
     }
-    
+
     protected function validateDocs($package)
     {
         $this->outln('Validate API docs.');
-        
+
         // remove previous validation records
         $target = "/tmp/phpdoc/{$package}";
         $this->shell("rm -rf {$target}");
-        
+
         // validate
         $cmd = "phpdoc -d src/ -t {$target} --force --verbose --template=xml";
         $line = $this->shell($cmd, $output, $return);
-        
+
         // remove logs
         $this->shell('rm -f phpdoc-*.log');
-        
+
         // get the XML file and look for errors
         $xml = simplexml_load_file("{$target}/structure.xml");
-        
+
         // are there missing @package tags?
         $missing = false;
         foreach ($xml->file as $file) {
-            
+
             // get the expected package name
             $class  = $file->class->full_name . $file->interface->full_name;
             $parts  = explode('\\', ltrim($class, '\\'));
             $expect = array_shift($parts) . '.' . array_shift($parts);
             $path = $file['path'];
-            
+
             // skip traits
             if (substr($path, -9) == 'Trait.php') {
                 continue;
@@ -142,7 +142,7 @@ abstract class AbstractCommand
                 $missing = true;
                 $this->outln("  Expected @package {$expect}, actual @package {$actual}, for {$path}");
             }
-            
+
             // class-level tag
             $actual = $file->class['package'] . $file->interface['package'];
             if ($actual != $expect) {
@@ -150,12 +150,12 @@ abstract class AbstractCommand
                 $this->outln("  Expected @package {$expect}, actual @package {$actual}, for {$class}");
             }
         }
-        
+
         if ($missing) {
             $this->outln('API docs not valid.');
             exit(1);
         }
-        
+
         // are there other invalidities?
         foreach ($output as $line) {
             // invalid lines have 2-space indents
@@ -164,7 +164,7 @@ abstract class AbstractCommand
                 exit(1);
             }
         }
-        
+
         // guess they're valid
         $this->outln('API docs look valid.');
     }
