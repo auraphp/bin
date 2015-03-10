1f2e47418243b329b6875e38724d0df7f0cbd0d3
Paul M. Jones <pmjones88@gmail.com>
2015-03-10 16:28:52 -0500

    nicer env failures, and add expectations

--- config/_env.php
+++ config/_env.php
@@ -1,6 +1,16 @@
 <?php
-$dotenv = new josegonzalez\Dotenv\Loader(
-    dirname(__DIR__) . DIRECTORY_SEPARATOR . '.env'
-);
-$dotenv->parse();
-$dotenv->toEnv();
+try {
+    $dotenv = new josegonzalez\Dotenv\Loader(
+        dirname(__DIR__) . DIRECTORY_SEPARATOR . '.env'
+    );
+    $dotenv->parse();
+    $dotenv->expect(array(
+        'AURA_CONFIG_MODE',
+        'AURA_BIN_GITHUB_USER',
+        'AURA_BIN_GITHUB_TOKEN',
+    ));
+    $dotenv->toEnv();
+} catch (Exception $e) {
+    echo $e->getMessage() . PHP_EOL;
+    exit(1);
+}
