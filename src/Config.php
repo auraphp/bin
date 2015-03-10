74c857861b6aec86968c3c4cbaeaf5eca119d836
Paul M. Jones <pmjones88@gmail.com>
2015-03-10 16:13:41 -0500

    add namespace, expect env vars prefixed with AURA_BIN_

--- src/Config.php
+++ src/Config.php
@@ -1,13 +1,21 @@
 <?php
+namespace Aura\Bin;
+
 class Config
 {
     protected $values = [];
-    
-    public function __construct(array $values = [])
+
+    public function __construct(array $env = [])
     {
-        $this->values = $values;
+        foreach ($env as $key => $val) {
+            if (substr($key, 0, 9) != 'AURA_BIN_') {
+                continue;
+            }
+            $key = strtolower(substr($key, 9));
+            $this->values[$key] = $val;
+        }
     }
-    
+
     public function __get($key)
     {
         if (isset($this->values[$key])) {
