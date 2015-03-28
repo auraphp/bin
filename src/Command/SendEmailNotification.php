dabf2635f2095c7a4bbb458f5759dcb844b53038
Hari KT <kthari85@gmail.com>
2015-03-29 00:14:57 +0530

    Alter email notifications with sending to ironmq. Later we can fetch and send via cron or what ever preferred way. This reduces the email floods into the group, and for the good.

---
+++ src/Command/SendEmailNotification.php
@@ -0,0 +1,68 @@
+<?php
+namespace Aura\Bin\Command;
+
+/**
+ * Sending email notification via IronMQ and later fetching the result and sending
+ */
+class SendEmailNotification extends AbstractCommand
+{
+    protected $ironmq;
+
+    protected $mailer;
+
+    public function __invoke()
+    {
+        $queue_name = 'notifications';
+        // Fetch 30 notifications
+        $messages = $this->ironmq->getMessages($queue_name, 30);
+        $message_ids = array();
+        $emailbody = '';
+        if (! empty($messages)) {
+            foreach ($messages as $message) {
+                $data = json_decode($message->body);
+                $emailbody .= "{$data->package} version {$data->version} < br />. Changes are <br />";
+                $emailbody .= $data->changes . " <br />";
+                $message_ids[] = $message->id;
+            }
+            if ($this->sendEmail($emailbody)) {
+                $this->ironmq->deleteMessages($queue_name, $message_ids);
+            }
+        }
+    }
+
+    protected function sendEmail($content)
+    {
+        $this->stdio->out('Notifying the mailing list ... ');
+
+        $to = 'auraphp@googlegroups.com';
+        $subject = "New Releases";
+        $body = <<<BODY
+Hi everyone!
+
+We have released
+
+{$content}
+
+Please let us know if you have any questions, comments, or concerns about this or any other release.  And thanks, as always, for supporting Aura!
+
+BODY;
+        $result = $this->mailer->send($to, $subject, $body);
+        if (! $result) {
+            $this->stdio->outln('failure.');
+            return false;
+        } else {
+            $this->stdio->outln('success.');
+            return true;
+        }
+    }
+
+    public function setIronMQ($ironmq)
+    {
+        $this->ironmq = $ironmq;
+    }
+
+    public function setMailer($mailer)
+    {
+        $this->mailer = $mailer;
+    }
+}
